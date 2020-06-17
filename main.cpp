8b28223e388238e8fdf742a90eb31561db95607f
sergioriv2 <62715874+sergioriv2@users.noreply.github.com>
2020-06-17 20:32:27 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,12 @@
 #include <iostream>
-#include <windows.h>
 #include <conio.h>
 #include "rlutil.h"
-#include <SFML/Graphics.hpp>
 
 using namespace std;
 using namespace rlutil;
 
 int main()
 {
-    locate(1, 7);
-    cout << "Hi";
 
-    return 0;
+   return 0;
 }
