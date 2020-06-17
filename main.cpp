a6dc6b8bb110dff2a161261122e014ee153c23ac
sergioriv2 <62715874+sergioriv2@users.noreply.github.com>
2020-06-17 20:32:06 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include "rlutil.h"
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace rlutil;
+
+int main()
+{
+    locate(1, 7);
+    cout << "Hi";
+
+    return 0;
+}
