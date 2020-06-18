3fd98be2ede870d0da187d443cd6b0be46b1449f
sergioriv2 <62715874+sergioriv2@users.noreply.github.com>
2020-06-17 21:00:32 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,55 @@
 #include <iostream>
 #include <conio.h>
 #include "rlutil.h"
+#include "funciones.h"
 
 using namespace std;
+using namespace rlutil;
+
 int main()
 {
 
-   return 0;
+int valF; // Lo que devuelve la flecha
+
+   while(true)
+    {
+        system("cls");
+    cout << "- Un jugador\n";
+    cout << "- Dos jugadores\n";
+    cout << "-Puntuacion maxima\n";
+    cout << "-Salir\n";
+
+    valF = selectDraw(20, 1, 4);
+
+    switch(valF)
+    {
+    case 1:
+        system("cls");
+        cout << "1\n";
+        system("pause");
+
+        break;
+    case 2:
+        system("cls");
+        cout << "2\n";
+        system("pause");
+        break;
+
+    case 3:
+        system("cls");
+        cout << "max\n";
+        system("pause");
+        break;
+    case 4:
+        return 0;
+        break;
+
+
+    }
+
+
+    }
+
+
+    return 0;
 }
