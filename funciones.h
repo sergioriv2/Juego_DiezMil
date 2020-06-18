ecd4ad85442f1eb170703d9afeb082658153383c
sergioriv2 <62715874+sergioriv2@users.noreply.github.com>
2020-06-17 22:06:04 -0300

    Update funciones.h
    Arregle un error de las flechas que si subias mas de lo que no podias se dibujaba otra flecha y era re molesto

--- funciones.h
+++ funciones.h
@@ -8,13 +8,22 @@
 using namespace std;
 using namespace rlutil;
 
-int selectDraw(int x, int y, int y2); // CON ESTO SE DIBUJA LAS FLECHAS DE LOS MENUS | x = POSICION EN X DE LAS FLECHAS, y = PRIMERA OPCION DEL MENU, y2 = ULTIMA OPCION DEL MENU
+///------------------- DECLARACION DE FUNCIONES----------------------
+
+int selectDraw(int x, int y, int y2); 
+// CON ESTO SE DIBUJA LAS FLECHAS DE LOS MENUS | x = POSICION EN X DE LAS FLECHAS, y = PRIMERA OPCION DEL MENU, y2 = ULTIMA OPCION DEL MENU
+
+///------------------- DESARROLLO DE FUNCIONES ----------------------
 
 int selectDraw(int x, int y, int y2){
-
 rlutil::setColor(rlutil::LIGHTBLUE);
 int val_Y = y, key = 1;
 
+rlutil::locate(x, val_Y);
+printf("%c", 174);
+
+   rlutil::locate(x, val_Y);
+cout << " ";
 rlutil::locate(x, val_Y);
 printf("%c", 174);
 
@@ -28,41 +37,37 @@ printf("%c", 174);
             val_Y++;
             rlutil::locate(x, val_Y);
             printf("%c", 174);
+            if(val_Y>y2)
+            {
+            rlutil::locate(x, val_Y);
+            cout << " ";
+            val_Y--;
+            rlutil::locate(x, val_Y);
+            printf("%c", 174);
+            }
             break;
+
         case 72:
-            rlutil::locate(x, val_Y);
+            rlutil::locate(x, val_Y);      
             cout << " ";
             val_Y--;
-            rlutil::locate(x, val_Y);
-            printf("%c", 174);
-            break;
-        }
-        if(val_Y>y2)
-        {
-            rlutil::locate(x, val_Y);
-            cout << " ";
-            val_Y--;
-            rlutil::locate(x, val_Y);
-            printf("%c", 174);
-        }
-        else
-        {
             if(val_Y<y)
             {
-                rlutil::locate(x, val_Y);
+                locate(x,val_Y);
                 cout << " ";
                 val_Y++;
-                rlutil::locate(x, val_Y);
+                locate(x, val_Y);
                 printf("%c", 174);
             }
+            rlutil::locate(x, val_Y);
+            printf("%c", 174);
+            break;
         }
-
     }
 
     setColor(WHITE);
     return val_Y; // SE DEVUELVE LA ULTIMA UBICACION DE LA FLECHA AL PONER ENTER
 
 }
-
 
 #endif // FUNCIONES_H_INCLUDED
