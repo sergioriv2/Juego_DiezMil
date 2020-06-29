15b48c0fa0eff759fb448aac6466f06bd4fa1545
sergioriv2 <62715874+sergioriv2@users.noreply.github.com>
2020-06-29 00:09:28 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,166 +10,198 @@ using namespace std;
 
 int main()
 {
+    int valorDados[6]; // DECLARACION DE VARIABLE PARA LOS DADOS
+    int valorFlechita; // Lo que devuelve la flecha
+    int puntuacion, puntuacion2, ronda2, lanzamiento, ronda, rondamax, Val = 0;
+    char listanombres[10][15], nombre1[15], nombre2[15];
+    int puntajes[10] = {0};
+    bool jugar;
 
-int valorFlechita; // Lo que devuelve la flecha
+    limpiarListaNombres(listanombres);
 
-bool jugar = true; // DECLARACION DE VARIABLES GENERALES
-char nombre1[20], nombre2[20], nombres[5][20];
-int ronda = 0, rondamax, lanzamiento = 0, puntuacion = 0, puntuacionM, puntuacionG[5];
-
-int valorDados[6]; // DECLARACION DE VARIABLE PARA LOS DADOS
-
-   while(true)
+    while(true)
     {
-    system("cls");
-    borderDraw();
-    locate()
-    cout << "- Un jugador\n";
-    cout << "- Dos jugadores\n";
-    cout << "- Puntuacion maxima\n";
-    cout << "- Salir\n";
 
-    valorFlechita = selectDraw(25, 1, 4);
-
-    switch(valorFlechita) // MENU
-    {
-    case 1:             // OPCION DE UN JUGADOR
-        system("cls");
-
-        borderDraw();   // SE DIBUJAN LOS BORDES DE PANTALLA
-
-        rondamax = unJugador(nombre1);    // FUNCION DE UN JUGADOR
-
-
-    underlineDraw(2, 3, 115);
-
-    locate(3, 2);
-    cout << "TURNO DE: ";
-    setColor(GREEN);
-    cout << nombre1;
-    setColor(WHITE);
-    locate(26, 2);
-    cout << "|\tRONDA N: ";
-    setColor(GREEN);
-    cout << ronda;
-    setColor(WHITE);
-    locate(49, 2);
-    cout << "|\tLANZAMIENTO N: ";
-    setColor(GREEN);
-    cout << lanzamiento;
-    setColor(WHITE);
-    locate(77, 2);
-    cout << "|\tPUNTUACION ACTUAL: ";
-    setColor(GREEN);
-    cout << puntuacion;
-    setColor(WHITE);
-
-    dibujoDados(4);
-
-    locate(6, 20);
-    cout << "Lanzar dados";
-
-    while(jugar)
-    {
-        for(int i=1; i<=rondamax; i++)
+        if(Val>=10)
         {
-            dibValorDados(ronda, valorDados);
-
-        valorFlechita = selectDraw(20, 20, 21);
-
-        switch(valorFlechita)
-        {
-
-        case 20:
-
-            ronda++;
-            lanzamiento++;
-
-            locate(42, 2);
-            setColor(GREEN);
-            cout << ronda;
-            locate(72, 2);
-            cout << lanzamiento;
-            setColor(WHITE);
-
-
-            limpiarDados();
-
-            tirarDados(valorDados, 1);
-
-            for(int i=0; i<6; i++)
-            {
-                switch(valorDados[i])
-                {
-                case 1:
-                    dibujar1(i);
-                    break;
-                case 2:
-                    dibujar2(i);
-                    break;
-                case 3:
-                    dibujar3(i);
-                    break;
-                case 4:
-                    dibujar4(i);
-                    break;
-                case 5:
-                    dibujar5(i);
-                    break;
-                case 6:
-                    dibujar6(i);
-                    break;
-                }
-            }
-
-                    locate(6,23);
-                    cout << "(Valores del vector de dados): ";
-                    locate(6, 25);
-
-                    mostrarDados(valorDados);
-
-                    getch();
-
-                    break;
-                }
+            Val = 0;
         }
 
-        jugar = false;
+        system("cls");
+
+        bordes(2);
+
+        menuPrincipal();
+
+        valorFlechita = selectDraw(27, 6, 12, 2);
+
+        switch(valorFlechita) // MENU
+        {
+        case 6:             // OPCION DE UN JUGADOR
+            system("cls");
+            jugar = true;
+            bordes(1);   // SE DIBUJAN LOS BORDES DE PANTALLA
+
+            rondamax = unJugador(nombre1);
+
+            bordes(1);
+
+            puntuacion = 0;
+            // ----------
+            ronda = 0;
+            // ----------
+            lanzamiento = 0;
+            // ----------
+
+
+            underlineDraw(2, 3, 115);
+
+            locate(3, 2);
+            cout << "TURNO DE: ";
+            setColor(GREEN);
+            cout << nombre1;
+            setColor(WHITE);
+            locate(26, 2);
+            cout << "|\tRONDA N: ";
+            setColor(GREEN);
+            cout << ronda;
+            setColor(WHITE);
+            locate(49, 2);
+            cout << "|\tLANZAMIENTO N: ";
+            setColor(GREEN);
+            cout << lanzamiento;
+            setColor(WHITE);
+            locate(77, 2);
+            cout << "|\tPUNTUACION ACTUAL: ";
+            setColor(GREEN);
+            cout << puntuacion;
+            setColor(WHITE);
+
+            dibujoDados(4);
+
+            locate(6, 20);
+            cout << "Lanzar dados";
+
+
+            while(jugar)
+            {
+                for(int i=0; i<=rondamax; i++)
+                {
+
+                    dadosPrimeraRonda(ronda, valorDados);
+
+                    locate(42, 2);
+                    setColor(GREEN);
+                    cout << ronda;
+                    locate(72, 2);
+                    cout << lanzamiento;
+                    locate(100, 2);
+                    cout << puntuacion;
+                    setColor(WHITE);
+
+                    valorFlechita = selectDraw(20, 20, 21, 1);
+
+                    switch(valorFlechita)
+                    {
+
+                    case 20:
+
+                        ronda++;
+                        lanzamiento++;
+
+                        limpiarDados();
+
+                        tirarDados(valorDados, 1); /// INGRESAR DADOS MANUALMENTE PARA TESTEAR 0, DADOS AL AZAR 1
+
+                        for(int i=0; i<6; i++)
+                        {
+                            switch(valorDados[i])
+                            {
+                            case 1:
+                                dibujar1(i);
+                                break;
+                            case 2:
+                                dibujar2(i);
+                                break;
+                            case 3:
+                                dibujar3(i);
+                                break;
+                            case 4:
+                                dibujar4(i);
+                                break;
+                            case 5:
+                                dibujar5(i);
+                                break;
+                            case 6:
+                                dibujar6(i);
+                                break;
+                            }
+                        }
+
+                        locate(6,23);
+                        cout << "(Valores del vector de dados): ";
+                        locate(6, 24);
+                        mostrarDados(valorDados);
+
+                        locate(24, 20);
+
+                        puntuacion += tresPares(valorDados);
+
+
+                        break;
+                    }
+
+                    jugar = false;
+
+                }
 
             }
 
-        system("pause");
+            ///----------------------------------
+            // PUNTUACION FINAL
+            strcpy(listanombres[Val], nombre1);
+            puntajes[Val] = puntuacion;
+            Val++;
 
-        break;
-    case 2:           // OPCION DE DOS JUGADORES
-        system("cls");
+            break;
+        case 8:           // OPCION DE DOS JUGADORES
+            system("cls");
 
-        borderDraw();   // SE DIBUJAN LOS BORDES DE PANTALLA
+            bordes(1);   // SE DIBUJAN LOS BORDES DE PANTALLA
 
-        /*dosJugadores(); // FUNCION DE DOS JUGADORES*/
+             ///----------------------------------
+            // PUNTUACION FINAL
+            strcpy(listanombres[Val], nombre1);
+            puntajes[Val] = puntuacion;
+            Val++;
 
-        system("pause");
-        break;
+            Val+=2;
 
-    case 3:         // OPCION DE MAX PUNTUACION
-        system("cls");
+            system("pause");
+            break;
 
-        borderDraw();
+        case 10:         // OPCION DE MAX PUNTUACION
+            system("cls");
 
-        /*puntuacionG = puntuacionMax()*/
+            bordes(1);
+
+            locate(5, 5);
+
+            for(int i=0; i<10; i++)
+            {
+                cout << i+1 << ". "<< listanombres[i];
+                cout << "\t\t" << puntajes[i] <<"\n";
+            }
 
 
-        system("pause");
-        break;
-    case 4:         // SALIR
-        return 0;
-        break;
+            system("pause");
+            break;
+        case 12:         // SALIR
+            return 0;
+            break;
 
-
+        }
     }
-
-    }
-
 
     return 0;
 }
